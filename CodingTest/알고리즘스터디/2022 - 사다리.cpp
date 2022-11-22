a64c67a054cdb6e5e2b9bce69b9f2bc70efee5bf
JK <alswkdrb1@naver.com>
2022-11-22 21:32:11 +0900

    [221122] 2022 - 사다리

--- CodingTest/알고리즘스터디/2022 - 사다리.cpp
+++ CodingTest/알고리즘스터디/2022 - 사다리.cpp
@@ -1,1 +1,55 @@
-﻿
+﻿//뭔가 단단히 잘못됐다...
+//다시 풀지말자
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double x, y, z;
+
+double binarySearch(double smallNum) {
+	double st = 0;
+	double end = smallNum;
+
+	int i = 0;
+	while (i++ <= 10000) {
+		double base = (st + end) / 2;
+		double h1 = sqrt(pow(x, 2) - pow(base, 2));
+		double h2 = sqrt(pow(y, 2) - pow(base, 2));
+		double expect = (h1 * h2) / (h1 + h2);
+		//cout << st << ' ' << end << ' ' << base << ' ' << expect << '\n';
+		if (z > expect)
+			end = base;
+		else
+			st = base;
+		/*if (expect + pow(10, -3) <= z)
+			end = base - 0.0005;
+		else if (expect - pow(10, -3) >= z)
+			st = base + 0.0005;
+		else
+			return base;*/
+	}
+	return st;
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> x >> y >> z;
+
+	double bigNum = x < y ? x : y;
+
+	cout.precision(3);
+	cout << fixed;
+	cout << binarySearch(bigNum);
+}
+
+/*
+3000000000
+3000000000
+1500000000
+*/
+
+//1430.511
+//908093.438

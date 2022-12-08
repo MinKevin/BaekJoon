ee503e1e119bb56cf16f5cbd4105393e8cbb6d31
JK <alswkdrb1@naver.com>
2022-12-09 05:02:58 +0900

    [221208] 15661 - 링크와 스타트 & 621.Task Scheduler

---
+++ CodingTest/알고리즘스터디/15661 - 링크와 스타트.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int N;
+int board[20][20];
+bool vis[20];
+int answer = 0x7FFFFFFF;
+
+int calculateStats() {
+	int homeStats = 0;
+	int awayStats = 0;
+	for (int i = 0; i < N - 1; i++) {
+		for (int j = i + 1; j < N; j++) {
+			if (!vis[i] && !vis[j]) {
+				awayStats += board[i][j] + board[j][i];
+			}
+			else if (vis[i] && vis[j]) {
+				homeStats += board[i][j] + board[j][i];
+			}
+		}
+	}
+	return abs(homeStats - awayStats);
+}
+
+void dfs(int idx, int cnt) {
+	if (cnt > N / 2)
+		return;
+
+	if (idx != 0)
+		answer = min(calculateStats(), answer);
+
+	for (int i = idx; i < N; i++) {
+		vis[i] = true;
+		dfs(i + 1, cnt + 1);
+		vis[i] = false;
+	}
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> board[i][j];
+		}
+	}
+
+	dfs(0, 0);
+
+	cout << answer;
+}

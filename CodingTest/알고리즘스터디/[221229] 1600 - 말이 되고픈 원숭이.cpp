0251a37880d8f0e3c2e25ec82ad67aceea93ca80
장규 민 <alswkdrb1@naver.com>
2022-12-29 15:56:44 +0900

    add [221229] 1600 - 말이 되고픈 원숭이

---
+++ CodingTest/알고리즘스터디/[221229] 1600 - 말이 되고픈 원숭이.cpp
@@ -0,0 +1,75 @@
+/*
+Date : 22.12.29
+Time : 15:25
+Comment : 
+*/
+
+#include <iostream>
+#include <queue>
+#include <tuple>
+
+using namespace std;
+
+int K, W, H;
+int board[200][200];
+int vis[200][200][31];
+int dx[12] = { 0, 0, 1, -1, 1, 1, 2, 2, -1, -1, -2, -2 };
+int dy[12] = { 1, -1, 0, 0, -2, 2, -1, 1, -2, 2, -1, 1 };
+int bfs() {
+	queue<tuple<int, int, int>> q;
+	q.push({ 0, 0, 0 });
+	vis[0][0][0] = 1;
+
+	while(!q.empty()) {
+		if (W == 1 && H == 1) {
+			return 0;
+		}
+
+		tuple<int, int, int> cur = q.front();
+		q.pop();
+
+		for (int dir = 0; dir < 12; dir++) {
+			int nx = get<0>(cur) + dx[dir];
+			int ny = get<1>(cur) + dy[dir];
+
+			if (nx < 0 || ny < 0 || nx >= H || ny >= W)
+				continue;
+
+			if (nx == H - 1 && ny == W - 1) {
+				if (dir < 4)
+					return vis[get<0>(cur)][get<1>(cur)][get<2>(cur)];
+				else if (get<2>(cur) < K)
+					return vis[get<0>(cur)][get<1>(cur)][get<2>(cur)];
+			}
+
+			if (dir < 4) {
+				if (board[nx][ny] == 0 && vis[nx][ny][get<2>(cur)] == 0) {
+					vis[nx][ny][get<2>(cur)] = vis[get<0>(cur)][get<1>(cur)][get<2>(cur)] + 1;
+					q.push({ nx, ny, get<2>(cur) });
+				}
+			}
+			else {
+				if (get<2>(cur) < K && board[nx][ny] == 0 && vis[nx][ny][get<2>(cur) + 1] == 0) {
+					vis[nx][ny][get<2>(cur) + 1] = vis[get<0>(cur)][get<1>(cur)][get<2>(cur)] + 1;
+					q.push({ nx, ny, get<2>(cur) + 1 });
+				}
+			}
+		}
+	}
+	return -1;
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> K >> W >> H;
+
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < W; j++) {
+			cin >> board[i][j];
+		}
+	}
+
+	cout << bfs();
+}

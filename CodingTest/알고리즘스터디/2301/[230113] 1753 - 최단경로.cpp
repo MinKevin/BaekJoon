1e09150faeb83fbd392a67fe9889713c1d656072
장규 민 <alswkdrb1@naver.com>
2023-01-17 08:26:08 +0900

    update

--- CodingTest/알고리즘스터디/2301/[230113] 1753 - 최단경로.cpp
+++ CodingTest/알고리즘스터디/2301/[230113] 1753 - 최단경로.cpp
@@ -3,48 +3,53 @@
 #include <queue>
 #include <algorithm>
 #include <limits.h>
+#define INF 987654321
+
 using namespace std;
 
 int V, E, K;
-vector<pair<int, int>> node[20001];
-priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-int val[20001];
+vector<pair<int, int>> node[20005];
+int val[20005];
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
 	cin >> V >> E >> K;
+
 	int u, v, w;
 	for (int i = 0; i < E; i++) {
 		cin >> u >> v >> w;
-		node[u].push_back({ v, w });
+		node[u].push_back(make_pair(v, w));
 	}
-	fill(&val[0], &val[V + 1], INT_MAX);
+
+	for (int i = 1; i <= V; i++) {
+		val[i] = INT_MAX;
+	}
 	val[K] = 0;
-	pq.push({ 0, K });
+	pq.push(make_pair(K, 0));
 
 	while (!pq.empty()) {
-		int x = pq.top().first;
-		int U = pq.top().second;
+		int loc = pq.top().first;
+		int sumDis = pq.top().second;
 		pq.pop();
+		cout << loc << ' ' << sumDis << '\n';
+		for (int i = 0; i < node[loc].size(); i++) {
+			int nextLoc = node[loc][i].first;
+			int w = node[loc][i].second;
 
-		int min = INT_MAX;
-		for (int i = 0; i < node[U].size(); i++) {
-			int v = node[U][i].first;
-			int w = node[U][i].second;
-			if (x + w < min)
-				min = x + w;
-		}
-		if (min < val[v]) {
-			val[v] = x + w;
-			pq.push(make_pair(x + w, v));
+			if (sumDis + w < val[nextLoc]) {
+				val[nextLoc] = sumDis + w;
+				pq.push(make_pair(nextLoc, sumDis + w));
+			}
 		}
 	}
 
 	for (int i = 1; i <= V; i++) {
-		if (val[i] == INT_MAX)
+		if (val[i] == INT_MAX) {
 			cout << "INF\n";
+		}
 		else
 			cout << val[i] << '\n';
 	}
